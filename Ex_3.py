ea63adcd208c58cc4f58826b9ca3ae09f5e12fc8
Loka-Koneva <loka.koneva@gmail.com>
2020-09-04 22:48:03 +0500

    Final changes of 'Ex_3'

--- Ex_3.py
+++ Ex_3.py
@@ -15,25 +15,48 @@
 Отправьте одного из солдат первого героя следовать за ним. 
 Выведите на экран идентификационные номера этих двух юнитов.
 """
+from random import randint
 
 class Person:
-    def __init__ (self, my_number, my_team=1):
-        self.my_number = my_number
-        self.my_team = my_team
+    count = 0
+    def __init__ (self, team):
+        self.number = Person.count
+        Person.count += 1
+        self.team = team
     
 class Hero(Person):
-    def __init__ (self, my_number, my_team, my_level = 1):
-        Person.__init__ (self, my_number, my_team)
-        self.my_level = my_level
-    def level (self):
-        self.my_level +=1
+    def __init__ (self, team):
+        Person.__init__ (self, team)
+        self.level = 1
+    def upLevel (self):
+        self.level +=1
 
 class Soldier(Person):
+    def __init__ (self, team):
+        Person.__init__(self, team)
+        self.my_hero = None
     def follow_to_Hero(self, hero):
-        self.hero = hero
+        self.my_hero = hero.number
 
-hero_1 = Hero (1, 1)
-hero_2 = Hero (2, 2)
+hero_1 = Hero(1)
+hero_2 = Hero(2)
 
 team_1 = []
 team_2 = []
+
+for i in range (20):
+    n = randint(1, 2)
+    if n==1:
+        team_1.append(Soldier(n))
+    else:
+        team_2.append(Soldier(n))
+
+print(len(team_1), len(team_2))
+
+if len(team_1)>len(team_2):
+    hero_1.upLevel()
+else:
+    hero_2.upLevel()
+
+team_1[0].follow_to_Hero(hero_1)
+print(team_1[0].number, hero_1.number)
